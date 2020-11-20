68821c7726b8742db3eceb73fdf84acf8463abd7
davidrocha9 <up201806371@fe.up.pt>
2020-11-20 09:58:58 +0000

    Added Products View to Home Page

--- confmate/lib/pages/homePage.dart
+++ confmate/lib/pages/homePage.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../Product.dart';
 import '../Profile.dart';
 import '../Talk.dart';
+import '../pages/productsPage.dart';
 
 class HomePage extends StatefulWidget {
   HomePage(this.profile);
@@ -53,7 +55,7 @@ class _HomePageState extends State<HomePage> {
         "assets/queiros.png", "Fds mén")
   ];
 
-  List<Talk> talksList = [
+  static List<Talk> talksList = [
     Talk(
         0,
         "Paysafes as Global Currency",
@@ -86,6 +88,57 @@ class _HomePageState extends State<HomePage> {
         12,
         new List<int>(),
         "assets/queiros.png")
+  ];
+
+  List<Product> productList = [
+    Product(
+        "Paysafes roubados",
+        0,
+        "Paysafes scammados pelo mitico #vski no Metin. Não garantimos que ainda tenham saldo, provavelmente foram totalmente depositados em apostas no CS ou em FIFA Points.",
+        0,
+        "assets/paysafe.jpg",
+        "For Students",
+        talksList[0]),
+    Product(
+        "Mochila da W33D",
+        5,
+        "Mochila W33D on the bankz #VSKI, mas manos, W33D é só nome da marca, não é para fumar manos",
+        0,
+        "assets/mochila.jpg",
+        "For Vskizinhos",
+        talksList[0]),
+    Product(
+        "FIFA 21",
+        1,
+        "Conta do Caio de FIFA visto que ele aziou tanto que partiu o PC e não pode jogar mais. Para ajudar a EA a fazer o mesmo jogo todos os anos, preço especial de 120€",
+        120,
+        "assets/fifa.jpg",
+        "For Pepegas",
+        talksList[2]),
+    Product(
+        "Microfone do Queiros",
+        2,
+        "Apenas funciona na perfeição no Ubuntu. Melhor microfone de ear rape alguma vez visto. CAAAAAAAAAAAARLOOOOOOOOOS TAS ME A OUVIIIIIIIIIIIIIIIIIR",
+        50,
+        "assets/microfone.jpg",
+        "For Big Kings",
+        talksList[3]),
+    Product(
+        "Improviso pos meus subs 2",
+        3,
+        "Após o grande sucesso do primeiro improviso, Tiagovski volta a dar cartas com o seu mais recente single. Dedicado especialmente aos àters, mas os subs continuam no coração. Video exclusivo",
+        25,
+        "assets/videovski.jpg",
+        "For researchers",
+        talksList[1]),
+    Product(
+        "Patanisks The Cat",
+        4,
+        "Patanisks, what are you doing here? No, Patanisks, go play, don't jump on top of my computer",
+        25,
+        "assets/cat.jpg",
+        "For researchers",
+        talksList[3])
   ];
 
   @override
@@ -141,23 +194,32 @@ class _HomePageState extends State<HomePage> {
                     fontSize: 17.0,
                     color: Color(0xFF473D3A)),
               ),
-              Padding(
-                padding: const EdgeInsets.only(right: 15.0),
-                child: Text(
-                  'See all',
-                  style: TextStyle(
-                      fontFamily: 'varela',
-                      fontSize: 15.0,
-                      color: Color(0xFFCEC7C4)),
-                ),
+            ],
+          ),
+          SizedBox(height: 15.0),
+          Container(
+              height: 350.0,
+              child: ListView(scrollDirection: Axis.horizontal, children: [
+                for (Talk x in talksList) _talkListCard(x),
+              ])),
+          SizedBox(height: 25.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                'Featured Talks',
+                style: TextStyle(
+                    fontFamily: 'varela',
+                    fontSize: 17.0,
+                    color: Color(0xFF473D3A)),
               ),
             ],
           ),
           SizedBox(height: 15.0),
           Container(
-              height: 410.0,
+              height: 350.0,
               child: ListView(scrollDirection: Axis.horizontal, children: [
-                for (Talk x in talksList) _talkListCard(x),
+                for (Product x in productList) _productListCard(x),
               ])),
         ]));
   }
@@ -229,6 +291,80 @@ class _HomePageState extends State<HomePage> {
                               borderRadius: BorderRadius.circular(200.0),
                               image: DecorationImage(
                                   image: AssetImage(talk.photo),
+                                  fit: BoxFit.cover))))
+                ]),
+              ],
+            )));
+  }
+
+  _productListCard(Product product) {
+    return Padding(
+        padding: EdgeInsets.only(left: 15.0, right: 15.0),
+        child: Container(
+            height: 300.0,
+            width: 225.0,
+            child: Column(
+              children: <Widget>[
+                Stack(children: [
+                  Container(height: 335.0),
+                  Positioned(
+                      top: 75.0,
+                      child: Container(
+                          padding: EdgeInsets.only(left: 10.0, right: 20.0),
+                          height: 260.0,
+                          width: 225.0,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25.0),
+                              color: Colors.blue[200]),
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                SizedBox(
+                                  height: 50.0,
+                                ),
+                                Text(
+                                  product.audience,
+                                  style: TextStyle(
+                                      fontFamily: 'nunito',
+                                      fontSize: 14.0,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white),
+                                ),
+                                SizedBox(height: 5.0),
+                                Text(
+                                  product.name,
+                                  style: TextStyle(
+                                      fontFamily: 'varela',
+                                      fontSize: 30.0,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white),
+                                ),
+                              ]))),
+                  Positioned(
+                      right: 5.0,
+                      bottom: 10.0,
+                      child: FlatButton.icon(
+                        textColor: Color(0xFF6200EE),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      productDescription(product)));
+                        },
+                        icon: Icon(Icons.arrow_forward_ios, size: 18),
+                        label: Text("MORE DETAILS"),
+                      )),
+                  Positioned(
+                      left: 70.0,
+                      top: 25.0,
+                      child: Container(
+                          height: 90.0,
+                          width: 90.0,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(200.0),
+                              image: DecorationImage(
+                                  image: AssetImage(product.picture),
                                   fit: BoxFit.cover))))
                 ]),
               ],
